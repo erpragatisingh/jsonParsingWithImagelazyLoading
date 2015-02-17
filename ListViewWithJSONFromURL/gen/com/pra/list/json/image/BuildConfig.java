4245e09b1930baa5633d357da71eecc6be71dbe3
Pragati Singh <pragati.singh@ireslab.com>
2015-02-17 17:58:48 +0530

    initial

---
+++ ListViewWithJSONFromURL/gen/com/pra/list/json/image/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.pra.list.json.image;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ListViewWithJSONFromURL/src/com/pra/list/json/image/JsonLog.java
@@ -0,0 +1,108 @@
+package com.pra.list.json.image;
+
+public class JsonLog {
+	/*{
+	    "countries": [
+	        {
+	            "countryname": "India",
+	            "flag": "http://ireslab.in/p/demo1/india.png",
+	            "language": "Hindi",
+	            "capital": "New Delhi",
+	            "currency": {
+	                "code": "INR",
+	                "currencyname": "Rupee"
+	            }
+	        },
+	        {
+	            "countryname": "Pakistan",
+	            "flag": "http://ireslab.in/p/demo1/pakistan.png",
+	            "language": "Urdu",
+	            "capital": "Islamabad",
+	            "currency": {
+	                "code": "PKR",
+	                "currencyname": "Pakistani Rupee"
+	            }
+	        },
+	        {
+	            "countryname": "Sri Lanka",
+	            "flag": "http://ireslab.in/p/demo1/srilanka.png",
+	            "language": "Sinhala",
+	            "capital": "Sri Jayawardenapura Kotte",
+	            "currency": {
+	                "code": "SKR",
+	                "currencyname": "Sri Lankan Rupee"
+	            }
+	        },
+	        {
+	            "countryname": "China",
+	            "flag": "http://ireslab.in/p/demo1/china.png",
+	            "language": "Chineese",
+	            "capital": "Beijing",
+	            "currency": {
+	                "code": "CNY",
+	                "currencyname": "Renminbi"
+	            }
+	        },
+	        {
+	            "countryname": "Bangladesh",
+	            "flag": "http://ireslab.in/p/demo1/bangladesh.png",
+	            "language": "Bangla",
+	            "capital": "Dhaka",
+	            "currency": {
+	                "code": "BDT",
+	                "currencyname": "Taka"
+	            }
+	        },
+	        {
+	            "countryname": "Nepal",
+	            "flag": "http://ireslab.in/p/demo1/nepal.png",
+	            "language": "Nepal Bhasa",
+	            "capital": "Kathmandu",
+	            "currency": {
+	                "code": "NPR",
+	                "currencyname": "Napalese Rupee"
+	            }
+	        },
+	        {
+	            "countryname": "Afghanistan",
+	            "flag": "http://ireslab.in/p/demo1/afghanistan.png",
+	            "language": "Dari Persian",
+	            "capital": "Kabul",
+	            "currency": {
+	                "code": "AFN",
+	                "currencyname": "Afghani"
+	            }
+	        },
+	        {
+	            "countryname": "North Korea",
+	            "flag": "http://ireslab.in/p/demo1/nkorea.png",
+	            "language": "Korean",
+	            "capital": "Pyongyang",
+	            "currency": {
+	                "code": "KPW",
+	                "currencyname": "North Korean Won"
+	            }
+	        },
+	        {
+	            "countryname": "South Korea",
+	            "flag": "http://ireslab.in/p/demo1/skorea.png",
+	            "language": "Korean",
+	            "capital": "Seoul",
+	            "currency": {
+	                "code": "KRW",
+	                "currencyname": "South Korean Won"
+	            }
+	        },
+	        {
+	            "countryname": "Japan",
+	            "flag": "http://ireslab.in/p/demo1/japan.png",
+	            "language": "Japanese",
+	            "capital": "Tokyo",
+	            "currency": {
+	                "code": "JPY",
+	                "currencyname": "Yen"
+	            }
+	        }
+	    ]
+	}*/
+}
